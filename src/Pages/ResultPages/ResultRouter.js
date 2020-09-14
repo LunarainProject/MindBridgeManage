ab27276179f6122f1e20b7263b6d03d94c83241d
ChlorUpload <chlor.upload@gmail.com>
2020-09-14 18:58:03 +0900

    배우자 결과를 가져오는 테스트 코드 추가

--- src/Pages/ResultPages/ResultRouter.js
+++ src/Pages/ResultPages/ResultRouter.js
@@ -15,9 +15,14 @@ export default class ResultRouter extends React.Component {
   constructor(props) {
     super(props);
 
-    /*test*/
-    if (this.props.result != [] || this.props.result != undefined || this.props.result != null) {
-      this.result = [];
+    this.result = [];
+    this.spouseResult = [];
+  }
+
+  async componentDidMount() {
+    console.log('initial state: ', this.props.result);
+    /*test result data*/
+    if ((this.props.result || []).length == 0) {
       for (var i = 1; i < 10; i++) {
         for (var j = 1; j <= 2; j++) {
           this.result.push({
@@ -27,11 +32,63 @@ export default class ResultRouter extends React.Component {
           });
         }
       }
+      console.log(this.result);
     } else {
       this.result = this.props.result;
     }
 
-    console.log(this.props.pkgId, this.result)
+    /*test spouse data*/
+    let response;
+    let couple = false;
+    try {
+      response = await fetch(`http://gfs3456.cafe24.com/api/CheckCouple.php?pkg_id=${this.props.pkgId}`);
+    } catch (e) {
+      console.log('fetch error: ', e);
+
+      couple = true;
+    }
+
+    let responseJson;
+    try {
+      responseJson = await response?.json() ?? {};
+    } catch(e) {
+      console.log('json parse error: ', e);
+    }
+
+    // coupled survey 일 경우
+    if(responseJson?.couple == 'true' || couple) {
+      let response;
+      let spouse = [];
+      try {
+        response = await fetch(`http://gfs3456.cafe24.com/api/partnerTestResult2.php?access_token=${this.props.user_srl}&pkg_id=${this.props.pkgId}&count=${this.props.partnerCount}`)
+      } catch(e) {
+        console.log('fetch error: ', e);
+      }
+      try {
+        spouse = response?.json() ?? [];
+      } catch(e) {
+        console.log('json parse error', e);
+      }
+
+      /* test data */
+      if ((spouse || []).length == 0) {
+        for (var i = 1; i < 10; i++) {
+          for (var j = 1; j <= 2; j++) {
+            this.spouseResult.push({
+              page_number: i,
+              problem_number: j,
+              answer: (random() * 4 + 1).toFixed(0),
+            });
+          }
+        }
+      } else {
+        this.spouseResult = spouse;
+      }
+    }
+
+
+
+    console.log(this.props.pkgId, this.result, this.spouseResult)
   }
 
   render() {
@@ -44,6 +101,7 @@ export default class ResultRouter extends React.Component {
                 pkgId={this.props.pkgId}
                 content={this.props.content}
                 result={Object.values(this.result)}
+                spouseResult={this.spouseResult}
             ></MarryDiagnosis>
           ) 
 
